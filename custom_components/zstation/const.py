77fca16b29b4b2c98ac131d9b99157749897dd0d
Xeos <services@zstation-app.com>
2025-12-07 21:27:01 +0100

    Update const.py

--- custom_components/zstation/const.py
+++ custom_components/zstation/const.py
@@ -1,13 +1,9 @@
 """Constants for the Z-Station Bridge integration."""
 
 DOMAIN = "zstation"
-
-# API endpoints (optional but recommended)
 API_GET_DEVICES = "/api/zstation/getdevices"
 API_REFRESH_DEVICES = "/api/zstation/refresh_devices"
 API_EXECUTE_ACTION = "/api/zstation/execute_action"
 API_GET_ZONE = "/api/zstation/getzone"
-
-# Integration metadata
 INTEGRATION_NAME = "Z-Station Bridge"
 
