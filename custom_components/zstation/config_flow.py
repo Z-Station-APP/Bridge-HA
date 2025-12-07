ac5937280ad5db570b829e15ab4f699090cb842b
Xeos <services@zstation-app.com>
2025-12-07 21:37:14 +0100

    Update config_flow.py

--- custom_components/zstation/config_flow.py
+++ custom_components/zstation/config_flow.py
@@ -12,16 +12,12 @@ class ZStationConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     async def async_step_user(self, user_input=None):
         """Handle the initial step."""
         errors = {}
-
-        # First time: show form
         if user_input is None:
             return self.async_show_form(
                 step_id="user",
                 data_schema=vol.Schema({}),  # REQUIRED for UI
                 errors=errors
             )
-
-        # When the user submits the form
         return self.async_create_entry(
             title="Z-Station Bridge",
             data=user_input
