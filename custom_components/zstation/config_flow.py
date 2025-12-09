1edbeaf4765a10e85391766a466d1d4202c4af44
Xeos <services@zstation-app.com>
2025-12-09 22:53:33 +0100

    Update config_flow.py

--- custom_components/zstation/config_flow.py
+++ custom_components/zstation/config_flow.py
@@ -2,12 +2,8 @@ from homeassistant import config_entries
 from .const import DOMAIN
 
 class ZStationConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
-    """Handle the config flow for Z-Station Bridge."""
-
     VERSION = 1
-
     async def async_step_user(self, user_input=None):
-        """Create a config entry without user input."""
         return self.async_create_entry(title="Z-Station", data={})
 
 
