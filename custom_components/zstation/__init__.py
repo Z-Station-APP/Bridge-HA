41b154c4db676814ee37ca8a2ea81d0d4a12f24b
Xeos <services@zstation-app.com>
2025-12-11 23:00:05 +0100

    Update __init__.py

--- custom_components/zstation/__init__.py
+++ custom_components/zstation/__init__.py
@@ -1,7 +1,11 @@
 import logging
 import voluptuous as vol
+
 from homeassistant.config_entries import ConfigFlow, ConfigEntry
 from homeassistant.core import HomeAssistant
+
+from homeassistant.helpers import config_entry_oauth2_flow  
+
 from .const import DOMAIN
 from .zones_api import ZStationZonesView
 from .devices_api import ZStationDevicesView
@@ -10,21 +14,45 @@ from .execute_action_api import ZStationExecuteActionView
 
 _LOGGER = logging.getLogger(__name__)
 
+
 async def async_setup(hass: HomeAssistant, config: dict):
     return True
 
+
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
+
+    # ---------------------------
+    # OAuth2 client
+    # ---------------------------
+    config_entry_oauth2_flow.async_register_implementation(
+        hass,
+        DOMAIN,
+        config_entry_oauth2_flow.LocalOAuth2Implementation(
+            hass,
+            DOMAIN,
+            "https://zstation-app.com/mobile", 
+            "",                            
+            "/auth/authorize",              
+            "/auth/token",                  
+        ),
+    )
+    
     _register_api_views(hass)
+
     return True
 
+
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
+    """Unload a config entry."""
     return True
+
 
 def _register_api_views(hass: HomeAssistant):
     hass.http.register_view(ZStationZonesView(hass))
     hass.http.register_view(ZStationDevicesView(hass))
     hass.http.register_view(ZStationRefreshDevicesView(hass))
     hass.http.register_view(ZStationExecuteActionView(hass))
+
 
 class ZStationConfigFlow(ConfigFlow, domain=DOMAIN):
 
